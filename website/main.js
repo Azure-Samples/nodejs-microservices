5da169a0871f0cf2c90e49418e113871d529c20a
sinedied <noda@free.fr>
2022-12-15 16:39:13 +0100

    feat: initial website implementation

---
+++ website/main.js
@@ -0,0 +1,98 @@
+const API_URL = '/api';
+const sidesInput = document.getElementById('sides');
+const countInput = document.getElementById('count');
+const maxInput = document.getElementById('max');
+const resultDiv = document.getElementById('result');
+
+async function getUserSettings() {
+  const response = await fetch(`${API_URL}/settings`);
+  if (response.ok) {
+    const settings = await response.json();
+    sidesInput.value = settings.sides ?? 6;
+  } else {
+    resultDiv.innerHTML = 'Cannot load user settings';
+  }
+}
+
+async function saveUserSettings() {
+  const sides = sidesInput.value;
+  const response = await fetch(`${API_URL}/settings`, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ sides }),
+  });
+  if (response.ok) {
+    resultDiv.innerHTML = 'User settings saved';
+  } else {
+    resultDiv.innerHTML = `Cannot save user settings: ${response.statusText}`;
+  }
+}
+
+async function rollDices() {
+  const count = countInput.value;
+  const response = await fetch(`${API_URL}/rolls`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ count }),
+  });
+  if (response.ok) {
+    const json = await response.json();
+    resultDiv.innerHTML = json.result.join(', ');
+  } else {
+    resultDiv.innerHTML = `Cannot roll dices: ${response.statusText}`;
+  }
+}
+
+async function getRollHistory() {
+  const max = maxInput.value;
+  const response = await fetch(`${API_URL}/rolls/history?max=${max}`);
+  if (response.ok) {
+    const json = await response.json();
+    resultDiv.innerHTML = json.result.join(', ');
+  } else {
+    resultDiv.innerHTML = `Cannot get roll history: ${response.statusText}`;
+  }
+}
+
+async function getUser() {
+  try {
+    const response = await fetch(`/.auth/me`);
+    if (response.ok) {
+      const json = await response.json();
+      return json.clientPrincipal;
+    }
+  } catch {}
+  return undefined;
+}
+
+function login() {
+  window.location.href = `/.auth/login/github`;
+}
+
+function logout() {
+  window.location.href = `/.auth/logout`;
+}
+
+async function main() {
+  // Check if user is logged in
+  const user = await getUser();
+
+  if (user) {
+    // Load user settings
+    await getUserSettings();
+    
+    document.getElementById('app').hidden = false;
+    document.getElementById('user').innerHTML = user.userDetails;
+  } else {
+    document.getElementById('login').hidden = false;
+  }
+
+  // Setup event handlers
+  document.getElementById('loginButton').addEventListener('click', login);
+  document.getElementById('logoutButton').addEventListener('click', logout);
+  document.getElementById('saveButton').addEventListener('click', saveUserSettings);
+  document.getElementById('rollButton').addEventListener('click', rollDices);
+  document.getElementById('historyButton').addEventListener('click', getRollHistory);
+}
+
+main();
