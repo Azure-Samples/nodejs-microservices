bb83c40e1ed69e1677e14ed018f0be1202890df6
sinedied <noda@free.fr>
2022-12-15 19:00:40 +0100

    refactor: move scripts

--- scripts/create-projects.sh
+++ scripts/create-projects.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 ##############################################################################
 # Usage: ./create-samples.sh
-# Creates the projects folders.
+# Creates the projects folders
 ##############################################################################
 
+set -e
+cd $(dirname ${BASH_SOURCE[0]})
+
+cd..
 mkdir packages/
 cd packages
+
 npx @nestjs/cli new dice-api
 npx express-generator --no-view gateway-api
 npx fastify-cli generate settings-api
