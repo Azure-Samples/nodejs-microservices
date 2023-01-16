7a39f74022f279865f61b55b6eb7ced573f7e432
sinedied <noda@free.fr>
2023-01-16 12:51:16 +0100

    feat: add validation

--- packages/dice-api/src/rolls.controller.ts
+++ packages/dice-api/src/rolls.controller.ts
@@ -1,18 +1,27 @@
-import { Body, Controller, Get, Post, Query, Logger } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Query,
+  Logger,
+  ParseIntPipe,
+  DefaultValuePipe,
+} from '@nestjs/common';
 import { DbService } from './db.service';
 
 @Controller('rolls')
 export class RollsController {
-  private readonly logger = new Logger(DbService.name);
+  private readonly logger = new Logger(RollsController.name);
 
   constructor(private readonly db: DbService) {}
 
   @Post()
-  async rollDice(@Body() body: { sides: number }) {
-    this.logger.log(`Rolling dice [${body.sides}]}`);
-    const result = Math.ceil(Math.random() * body.sides);
+  async rollDice(@Body('sides', ParseIntPipe) sides: number) {
+    this.logger.log(`Rolling dice [sides: ${sides}]}`);
+    const result = Math.ceil(Math.random() * sides);
     await this.db.addRoll({
-      sides: body.sides,
+      sides: sides,
       timestamp: Date.now(),
       result,
     });
@@ -20,10 +29,11 @@ export class RollsController {
   }
 
   @Get('history')
-  async getHistory(@Query() query) {
-    const max = query.max ? Number(query.max) : 10;
-    const sides = query.sides ? Number(query.sides) : 6;
-    this.logger.log(`Retrieving last ${max} rolls history`);
+  async getHistory(
+    @Query('max', new DefaultValuePipe(10), ParseIntPipe) max: number,
+    @Query('sides', new DefaultValuePipe(6), ParseIntPipe) sides: number
+  ) {
+    this.logger.log(`Retrieving last ${max} rolls history [sides: ${sides}]`);
     const rolls = await this.db.getLastRolls(max, sides);
     return { result: rolls.map((roll) => roll.result) };
   }
